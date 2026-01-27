938c28f8ffcd3ff41f1017eb4cceb135bfedee63
GroupP04 <makaiojimenez@berkeley.edu>
2026-01-27 12:10:53 -0800

    First Piece of Lab1

---
+++ constant.py
@@ -0,0 +1,13 @@
+import ugradio
+import pandas as pd
+from rtlsdr import RtlSdr
+import asyncio
+import time
+import numpy as np
+
+sdr = ugradio.sdr.SDR(sample_rate=2.2e6)
+data = sdr.capture_data(2048, nblocks=10)
+print(sdr)
+print(data)
+np.savez("test25.npz", data)
+
